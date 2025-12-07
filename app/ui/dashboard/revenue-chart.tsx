b78478285c258a01390eed123be3a6e23daeec4b
Ali-shakib23 <alishakib2310@gmail.com>
2025-12-07 15:18:44 +0200

    commented some unwanted code that stoped databse configuation

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -32,16 +32,16 @@ export default async function RevenueChart({
 
       <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-          <div
+          {/* <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
           >
             {yAxisLabels.map((label) => (
               <p key={label}>{label}</p>
             ))}
-          </div>
+          </div> */}
 
-          {revenue.map((month) => (
+          {/* {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
                 className="w-full rounded-md bg-blue-300"
@@ -53,7 +53,7 @@ export default async function RevenueChart({
                 {month.month}
               </p>
             </div>
-          ))}
+          ))} */}
         </div>
         <div className="flex items-center pb-2 pt-6">
           <CalendarIcon className="h-5 w-5 text-gray-500" />
